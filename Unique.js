a5a46123fef9fa2ca3236526dfa8d8b41e0e2697
Porter <lporter@netsuite.com>
2018-01-28 16:13:29 -0600

    Setting up Unique formula and loop to check array

---
+++ Unique.js
@@ -0,0 +1,7 @@
+//01-28-18 script to detect if an array contains unique values//
+function isUnique(array){
+    var output = [];
+    var arrayLength = array.length;
+    for (var i=0; i < arrayLength; i++)
+        
+};
