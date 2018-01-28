726702724baa9304bedf0623b934075e545afc6a
Porter <lporter@netsuite.com>
2018-01-28 11:24:04 -0600

    Defining function and string loop

--- Reverse.js
+++ Reverse.js
@@ -1,6 +1,10 @@
 //01-28-18 Reverse script with function to turn words backwards//
 
 function ReverseString(string) {
-    var stringLength = string.length
-    
+    var stringLength = string.length;
+    var stringReverse = "";
+    for (var count=0; count < stringLength; count--)
+        
+    return stringReverse;
 }
+ReverseString("reverseme");
