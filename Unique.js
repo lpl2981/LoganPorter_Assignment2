bb8442802f3e19de61575feb8588c05ebee21a8e
Porter <lporter@netsuite.com>
2018-01-28 19:10:36 -0600

    Built unique script loop and indexOf checker

--- Unique.js
+++ Unique.js
@@ -2,6 +2,10 @@
 function isUnique(array){
     var output = [];
     var arrayLength = array.length;
-    for (var i=0; i < arrayLength; i++)
-        
-};
+    for (var i=0; i < arrayLength; i++) {
+      var check = array[i];
+        if (output.indexOf(check) === -1)
+            return false;
+    }
+}
+console.log(isUnique(4,5,6,2));
