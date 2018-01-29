62b6d14ca743ef57a1d5c4df897ba158d204f148
Porter <lporter@netsuite.com>
2018-01-28 21:58:23 -0600

    Changes to counts

--- deepEqual.js
+++ deepEqual.js
@@ -1,18 +1,16 @@
 //01-28-18 script to compare object property values//
 function deepEqual(object1, object2) {
-    var list1 = {};
-    var list2 = {};
     var counter1 = 0;
     var counter2 = 0;
     if (typeof (object1 === object2) && object1 != null && object2 != null)
         return true;
     else if (typeof object1 !== object2 || object1 == null || object2 == null)
         return false;
-for(list1 in object1)
+for(var list in object1)
     counter1 += 1;
-for(list2 in object2)
+for(var list in object2)
     counter2 += 1;
-    if (counter1 !== counter2)
+    if (!(list in object1)){
         return false;
     }
 }
