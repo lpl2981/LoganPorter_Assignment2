ca5bebeb4a027423c3f2e7305edaf48ff5bc637c
Porter <lporter@netsuite.com>
2018-01-28 21:48:50 -0600

    added blank objects to add from old and compare

--- deepEqual.js
+++ deepEqual.js
@@ -1,14 +1,18 @@
 //01-28-18 script to compare object property values//
 function deepEqual(object1, object2) {
+    var list1 = {};
+    var list2 = {};
     if (typeof (object1 === object2) && object1 != null && object2 != null)
         return true;
     else if (typeof object1 !== object2)
         return false;
-for(var counter1 in object1)
+for(list1 in object1)
     counter1 += 1;
-for(var counter2 in object2)
-    counter2 += 2;
-
+for(list2 in object2)
+    counter2 += 1;
+    if (counter1 !== counter2){
+        return false;
+    }
 }
 var obj = {here: {is: "an"}, object: 2};
 console.log(deepEqual(obj, obj));
