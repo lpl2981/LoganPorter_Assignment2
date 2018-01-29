b0e77c955aad41777cd8bf6b65cbf82144a55e8c
Porter <lporter@netsuite.com>
2018-01-28 21:11:00 -0600

    added console tests and else statement

--- deepEqual.js
+++ deepEqual.js
@@ -2,5 +2,14 @@
 function deepEqual(object1, object2) {
     if (typeof (object1 === object2) && object1 != null && object2 != null) {
         return true;
+    else if (typeof object1 != object2)
+        return false;
     }
 }
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
