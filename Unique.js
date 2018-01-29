e1ba4bd14f00762adf4aca991f8a0d59419aafc5
Porter <lporter@netsuite.com>
2018-01-28 19:25:02 -0600

    Trying a few different methods

--- Unique.js
+++ Unique.js
@@ -1,13 +1,13 @@
 //01-28-18 script to detect if an array contains unique values//
 function isUnique(array){
     var output = [];
-    var arrayLength = array.length;
-    for (var i=0; i < arrayLength; i++) {
-      var check = array[i];
+    var aLength = array.length;
+    for (var i=0; i < aLength; i++) {
+        var check = array[i];
         if (output.indexOf(check) === -1)
-            return false;
-        else
             return true;
+        else
+            return false;
     }
 }
-console.log(isUnique([4,5,6,2]));
+console.log(isUnique([4,5,6,4]));
