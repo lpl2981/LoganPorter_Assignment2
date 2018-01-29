a847466c0620293524e361b377806fbe38ca50a0
Porter <lporter@netsuite.com>
2018-01-28 21:00:17 -0600

    first attempt at function & test

---
+++ deepEqual.js
@@ -0,0 +1,6 @@
+//01-28-18 script to compare object property values//
+function deepEqual(object1, object2) {
+    if (typeof (object1 === object2) && object1 != null && object2 != null) {
+        return true;
+    }
+}
