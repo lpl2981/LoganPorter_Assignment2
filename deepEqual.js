02b6ae65720091d83b6c420636c43c7f46ece947
Porter <lporter@netsuite.com>
2018-01-28 22:15:43 -0600

    Fixed second if statement

--- deepEqual.js
+++ deepEqual.js
@@ -2,17 +2,18 @@
 function deepEqual(object1, object2) {
     var counter1 = 0;
     var counter2 = 0;
-    if (object1 === object2 && object1 != null && object2 != null)
+    if ((object1 === object2) && object1 != null && object2 != null)
         return true;
-    else if (typeof object1 !== object2 || object1 == null || object2 == null)
+    else if (typeof object1 !== "object" || object1 == null || typeof object2 !== "object" || object2 == null)
         return false;
 for(var list in object1)
     counter1 += 1;
-for(var list in object2)
+for(var list in object2) {
     counter2 += 1;
-    if (!(list in object1) || (!object1[list], object2[list])){
+    if (!(list in object1) || !deepEqual(object1[list], object2[list]))
         return false;
     }
+    return counter1 === counter2;
 }
 var obj = {here: {is: "an"}, object: 2};
 console.log(deepEqual(obj, obj));
